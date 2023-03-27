7d1071a74024d30be85836e0154bff5e56ec4f15
Maciej <40804130+Mieciej@users.noreply.github.com>
2023-03-27 21:56:28 +0200

    Print Header Info

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include "bmp.h"
 
+void print_header_info(BITMAPFILEHEADER *file_header, BITMAPINFOHEADER *info_header);
 
 int main(int argc, char const *argv[])
 {
-    FILE *file = fopen("tux.bmp","rb");
+    FILE *file = fopen("c.bmp","rb");
     if(file==NULL) 
     {
         printf("Failed to read a file.\n");
@@ -33,11 +34,7 @@ int main(int argc, char const *argv[])
     fread(&info_header.biClrImportant,4,1,file);
 
     fseek(file,info_header.biSize-40,SEEK_CUR);
-    LONG buff=0;
-    fread(&buff,2,1,file);
-    printf("%X\n",buff);
-
-
+    print_header_info(&file_header,&info_header);
         
 
 
@@ -47,4 +44,29 @@ int main(int argc, char const *argv[])
 
 
     return 0;
+}
+void print_header_info(BITMAPFILEHEADER *file_header, BITMAPINFOHEADER *info_header)
+{
+    printf("BITMAPFILEHEADER:\n");
+    printf("\tbfType:\t\t0x%X\t(%c%c)\n",file_header->bfType,(file_header->bfType) & 0xFF,(file_header->bfType>>8) & 0xFF);
+    printf("\tbfSize:\t\t%d\n",file_header->bfSize);
+    printf("\tbfReserved1:\t0x%X\n",file_header->bfReserved1);
+    printf("\tbfReserved2:\t0x%X\n",file_header->bfReserved2);
+    printf("\tbfOffBits:\t%u\n",file_header->bfOffBits);
+
+    printf("BITMAPINFOHEADER:\n");
+    printf("\tbiSize:\t\t%u\n",info_header->biSize);
+    printf("\tbiWidth:\t%d\n",info_header->biWidth);
+    printf("\tiHeight:\t%d\n",info_header->biHeight);
+    printf("\tbiPlanes:\t%u\n",info_header->biPlanes);
+    printf("\tbiBitCount:\t%u\n",info_header->biBitCount);
+    printf("\tbiCompression:\t%u\n",info_header->biCompression);
+    printf("\tbiSizeImage:\t%u\n",info_header->biSizeImage);
+    printf("\tbiXPelsPerMeter\t%d\n",info_header->biXPelsPerMeter);
+    printf("\tbiYPelsPerMeter\t%d\n",info_header->biYPelsPerMeter);
+    printf("\tbiClrUsed\t%u\n",info_header->biClrUsed);
+    printf("\tbiClrImportant\t%u\n",info_header->biClrImportant);
+
+
+
 }
